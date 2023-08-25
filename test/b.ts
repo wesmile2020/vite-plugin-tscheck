83765981f2a3199593d7311194c573756bf56d36
Li Lei <lei3.li@Lis-MacBook-Pro.local>
2023-08-25 18:41:10 +0800

    update: for test

--- test/b.ts
+++ test/b.ts
@@ -1,3 +1,8 @@
+import * as fs from 'fs';
+import * as child_process from 'node:child_process';
+
 const a: string = '1';
 
 const lngLat: LngLat = { lng: 1, lat: 1 };
+
+child_process.spawn('ls', [], {})
